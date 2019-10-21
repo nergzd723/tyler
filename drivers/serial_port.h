7b69d2323a9a883c6f07560f1ccdb1faa9752f0b
Mark <clashclanacc2602@gmail.com>
2019-10-21 18:01:04 +0300

    fix typos

--- drivers/serial_port.h
+++ drivers/serial_port.h
@@ -1,6 +1,6 @@
 #define SERIAL_COM1_BASE                0x3F8
 
-void serial_init(unsigned char com);
-void serial_write(unsigned char com, char * s);
-void serial_write_byte(unsigned char b);
+void serial_init(unsigned short com);
+void serial_write(unsigned short com, char * s);
+void serial_write_byte(unsigned short b);
 
--- kernel/arch/x86-c/serialport.c
+++ kernel/arch/x86-c/serialport.c
@@ -25,29 +25,29 @@ void serial_configure_baud_rate(unsigned short com, unsigned short divisor)
          divisor & 0x00FF);
 }
 
-void serial_configure_line(int16 com)
+void serial_configure_line(unsigned short com)
 {
     outb(SERIAL_LINE_COMMAND_PORT(com), 0x03);
 }
 
-void serial_configure_fifo(int16 com)
+void serial_configure_fifo(unsigned short com)
 {
     outb(SERIAL_FIFO_COMMAND_PORT(com), 0xC7);
 }
 
-void serial_configure_modem(int16 com)
+void serial_configure_modem(unsigned short com)
 {
     outb(SERIAL_MODEM_COMMAND_PORT(com), 0x03);
 }
 
-void serial_init(int16 com) {
+void serial_init(unsigned short com) {
     serial_configure_baud_rate(com, 2);
     serial_configure_line(com);
     serial_configure_fifo(com);
     serial_configure_modem(com);
 }
 
-void serial_write(int16 com, char * s) {
+void serial_write(unsigned short com, char * s) {
     int i = 0;
     while (s[i]) {
         // Block until buffer is not full
