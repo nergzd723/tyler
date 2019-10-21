27928b7c0cdfca548d09419e8dc6c7157a2dad2e
Mark <clashclanacc2602@gmail.com>
2019-10-21 17:47:55 +0300

    arghargh

--- kernel/arch/x86-c/irq.c
+++ kernel/arch/x86-c/irq.c
@@ -1,5 +1,7 @@
 #include "irq.h"
-void interrupt_handler() {
+#include "fbh.h"
+void interrupt_handler(struct cpu_state cpu, struct stack_state stack, uint32_t interrupt)
+{
   log("interrupt_handler()\n");
   return;
 } 
