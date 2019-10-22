827c71ff141999a3aaba7fe13fdc554f64071714
Mark <clashclanacc2602@gmail.com>
2019-10-22 16:06:46 +0300

    what if

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -12,9 +12,7 @@ int kernel_main(){
     clear_screen();
     serial_init(SERIAL_COM1_BASE);
     fb_write("Yes i know that the colorpalette is awful");
-    initialize_gdt();
     write(LOG, "GDT init ");
-    initialize_idt();
     log("IDT init ");
     pic_init();
     checkmate(FB_GREEN, FB_LIGHT_RED);
